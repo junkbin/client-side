a7d1e331b2268a5023a82e4e94cfc942e9ee10b9
santosh <santosh.ece06@gmail.com>
2019-10-05 11:51:11 +0530

    http.module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit { 
   title = 'client-side';
-  public list = [{
-    productId: '1',
-    productName: 'VIVO',
-    categoryId: 'V10',
-    categoryName: 'Mobile',
-  }, {
-    productId: '1',
-    productName: 'VIVO',
-    categoryId: 'V10',
-    categoryName: 'Mobile'
-  }];
+  public list = [];
+
+  public constructor(private http: HttpClient) { }
+
+
+  public ngOnInit() {
+
+    // this is hardcoded.
+    this.list = [{
+      productId: '1',
+      productName: 'VIVO',
+      categoryId: 'V10',
+      categoryName: 'Mobile',
+    }, {
+      productId: '1',
+      productName: 'VIVO',
+      categoryId: 'V10',
+      categoryName: 'Mobile'
+    }];
+
+    this.listProduct();
+  }
+
+
+  /** CALL API TO LIST THE PRODCUT */
+  public listProduct() {
+
+  }
+
+  /** Call API HERE FOR SERVER SIDE */
+  public addProduct() {
+    console.log('add.product.');
+  }
 }
